fad6b51506285aa59518dac8d2390d80c0abbf72
Sonickyle27 <sonickyle27+github@gmail.com>
2021-01-02 11:17:20 +0000

    Point out that the key is an example

--- linux.sh
+++ linux.sh
@@ -3,7 +3,7 @@
 # This takes text within quotes and sends it to DeepL to be translated.
 # You need an API key from DeepL that you can get from https://www.deepl.com/pro#developer
 
-apikey=77b8102f-d342-f26f-2fac-6daadae08d25 # Put your API key here!
+apikey=77b8102f-d342-f26f-2fac-6daadae08d25 # Put your API key here! This is an example!
 # Look at https://www.deepl.com/docs-api/translating-text/request/ for supported languages
 targetlang=EN
 sourcelang=JA
