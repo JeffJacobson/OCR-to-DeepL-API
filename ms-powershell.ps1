1b87e45d4b92c8fa76d355ab0e41e29121fbabcd
Sonickyle27 <sonickyle27+github@gmail.com>
2021-01-07 18:37:11 +0000

    Change file extension from .ps to .ps1

---
+++ ms-powershell.ps1
@@ -0,0 +1,10 @@
+<#  
+    Microsoft Powershell script created by Sonickyle27.
+    This takes text within quotes and sends it to DeepL to be translated.
+    You need an API key from DeepL that you can get from https://www.deepl.com/pro#developer
+#>
+
+$apikey="77b8102f-d342-f26f-2fac-6daadae08d25" # Put your API key here! This is an example!
+# Look at https://www.deepl.com/docs-api/translating-text/request/ for supported languages
+$sourcelang="JA"
+$targetlang="EN"
